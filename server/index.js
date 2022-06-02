fc5e393268bd62041ca9fd54d13cc5fff840d8c1
Filip <filip.sundholm@utb.ecutbildning.se>
2022-06-02 21:45:55 +0200

    mongo work in progress

--- server/index.js
+++ server/index.js
@@ -2,23 +2,6 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const friendsRouter = require('./routes/friendsRouter');
-const { MongoClient } = require('mongodb');
-
-const main = async () => {
-    const uri = `mongodb+srv://LordSund:q8qIiXCGI3q3YJDz@cluster0.jfhdz4h.mongodb.net/?retryWrites=true&w=majority`;
-
-    const client = new MongoClient(uri);
-
-    try {
-        await client.connect();
-    } catch (error) {
-        console.error(error);
-    } finally {
-        await client.close();
-    }
-}
-
-main().catch(console.error);
 
 app.use(cors());
 app.use(express.json());
--- server/routes/friendsRouter.js
+++ server/routes/friendsRouter.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const router = express.Router();
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASS}@cluster0.jfhdz4h.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    if (err) {
+        console.log(err)
+    }
+    const collection = client.db("labb").collection('Friends');
+
+});
 
 
 router.post('/add', async (req, res) => {
     try {
-        
+        const collection = client.db('labb').collection('Friends');
+
+        const target = req.body;
+
+        collection.insertOne(target, (err, response) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(response)
+            }
+        })
+        res.json(`${target} inserted OK.`)
     } catch (error) {
         console.log(error.message);
     }
+})
+
+router.get('/all', async (req, res) => {
+    const allFriends = client.db('labb').collection('Friends').find({});
+
+    console.log('------------HERE----------', allFriends);
+    res.json(allFriends);
 })
 
 
