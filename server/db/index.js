8d8493aa24b142854734281236d355e6839fcde0
Filip <filip.sundholm@utb.ecutbildning.se>
2022-06-09 10:20:13 +0200

    get all, delete and post working

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,3 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASS}@cluster0.jfhdz4h.mongodb.net/?retryWrites=true&w=majority`;
-
-let db;
-
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    if (err) {
-        console.log(err)
-    }
-    const collection = client.db("labb").collection('Friends');
-    
-    // perform actions on the collection object
-    client.close();
-});
--- server/routes/friendsRouter.js
+++ server/routes/friendsRouter.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const {ObjectId} = require('mongodb')
 const router = express.Router();
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASS}@cluster0.jfhdz4h.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = client.db('labb');
+
 client.connect(err => {
     if (err) {
         console.log(err)
     }
-    const collection = client.db("labb").collection('Friends');
-
+    console.log('Connected to DB.')
 });
 
 
 router.post('/add', async (req, res) => {
     try {
-        const collection = client.db('labb').collection('Friends');
+        const collection = db.collection('Friends');
 
         const target = req.body;
 
@@ -28,17 +30,37 @@ router.post('/add', async (req, res) => {
                 console.log(response)
             }
         })
-        res.json(`${target} inserted OK.`)
+        res.json(`${target.firstName} inserted OK.`)
     } catch (error) {
         console.log(error.message);
     }
 })
 
-router.get('/all', async (req, res) => {
-    const allFriends = client.db('labb').collection('Friends').find({});
+router.get('/', async (req, res) => {
+    try {
+        const friends = db.collection('Friends');
+        const allFriends = await friends.find({}).toArray((err, result) => {
+            if(err){
+                console.log(err)
+            }
+            res.json(result);
+        })
 
-    console.log('------------HERE----------', allFriends);
-    res.json(allFriends);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const collection = db.collection('Friends');
+        
+        collection.deleteOne({_id: ObjectId(req.params.id)})
+
+        res.json('Document deleted successfully.');
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
